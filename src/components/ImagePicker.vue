50b8818f2780daf36144e452533d4ba96fc76e31
darklordstudios <dan@darklordstudios.com>
2024-11-11 07:35:59 -0500

    Initial Repo

---
+++ src/components/ImagePicker.vue
@@ -0,0 +1,30 @@
+<!-- eslint-disable @typescript-eslint/no-unused-vars -->
+<script setup lang="ts">
+/*  eslint-disable @typescript-eslint/no-unused-vars */
+import { ref, onMounted, defineProps, defineModel } from 'vue'
+
+const props = defineProps<{
+  buttonLabel: string
+}>()
+
+const imagefield = defineModel('imagefield')
+
+function onSelectImage() {
+  alert('You clicked it buddy!')
+}
+
+onMounted(() => {
+  console.log('COMPONENT MOUNTED')
+})
+</script>
+
+<template>
+  <InputGroup>
+    <input v-model="imagefield" placeholder="Select an Image" />
+    <InputGroupAddon>
+      <Button icon="pi pi-image" v-on:click="onSelectImage" />
+    </InputGroupAddon>
+  </InputGroup>
+</template>
+
+<style scoped></style>
